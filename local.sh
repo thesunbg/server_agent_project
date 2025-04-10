eb8c11a2160abe5f409fa87be22da532c01938d8
donv <donv@inet.vn>
2025-04-10 22:15:22 +0700

    updated

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
 cd .. &&
-tar -czvf server_agent_project.tar.gz server_agent_project/ &&
+tar -czvf server_agent_project.tar.gz --exclude='.git' server_agent_project/ &&
 cp server_agent_project.tar.gz server_agent_project/ &&
 cd server_agent_project
